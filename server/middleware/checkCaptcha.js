a976892675b2a8b20776b8d35bfe1b3eb1002a2e
SahilAnower <sahilanower2@gmail.com>
2024-02-27 15:50:34 +0530

    Resolved errors of the application

--- server/middleware/checkCaptcha.js
+++ server/middleware/checkCaptcha.js
@@ -1,35 +1,40 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const checkCaptcha = async (req, res, next) => {
-  let token = req.header('Authorization');
+  let token = req.header("Authorization");
   if (!token) {
-    return res.status(403).send('Access Denied');
+    return res.status(403).send("Access Denied");
   }
-  if (token.startsWith('Bearer ')) {
+  if (token.startsWith("Bearer ")) {
     token = token.slice(7, token.length).trimLeft();
   }
 
   try {
     // Sending secret key and response token to Google Recaptcha API for authentication.
+    // console.log(process.env.SECRET_KEY);
     const response = await axios.post(
-      `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.SECRET_KEY}&response=${token}`
+      `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.SECRET_KEY}&response=${token}`,
+      {},
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
     );
 
     // Check response status and send back to the client-side
     if (response.data.success) {
       next();
     } else {
-      return res
-        .status(501)
-        .json({
-          error: response.data['error-codes']
-            ? response.data['error-codes'][0]
-            : 'Robot Caught! 🤖',
-        });
+      return res.status(501).json({
+        error: response.data["error-codes"]
+          ? response.data["error-codes"][0]
+          : "Robot Caught! 🤖",
+      });
     }
   } catch (error) {
     // Handle any errors that occur during the reCAPTCHA verification process
     console.error(error);
-    res.status(500).send('Error verifying reCAPTCHA');
+    res.status(500).send("Error verifying reCAPTCHA");
   }
 };
